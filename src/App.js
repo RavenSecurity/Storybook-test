af542801bd0e7b9d70208cdedb7ff3d065b67603
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-09-01 00:26:36 +0200

    button:hover

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import './App.css';
+import "./App.css";
 
-import { Button } from './stories/Button';
+import { Button } from "./stories/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button
-  label="Publish"
-  onClick={() => {}}
-  primary
-/>
-
+        <Button label="Publish" onClick={() => {}} primary />
       </header>
     </div>
   );
